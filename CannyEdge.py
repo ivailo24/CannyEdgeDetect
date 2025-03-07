447509c18671547f22af9359b11b982ded68db08
No Name <him@abv.com>
2025-03-07 10:58:12 +0200

    picture name changed to actual picture name

--- CannyEdge.py
+++ CannyEdge.py
@@ -8,7 +8,7 @@ img = cv.imread('maxresdefault.jpg', cv.IMREAD_GRAYSCALE)
 
 assert img is not None, "file could not be read, check with os.path.exists()"
 edges = cv.Canny(img,100,200)
-plt.subplot(121),plt.imshow(img,cmap = 'gray')
+plt.subplot(121),plt.imshow(img,cmap = 'gray')git
 plt.title('Original Image'), plt.xticks([]), plt.yticks([])
 plt.subplot(122),plt.imshow(edges,cmap = 'gray')
 plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
